119065b01129ee7842bb78f54c29dea236205df1
Anubis Capstone <anubiscapstone@gmail.com>
2015-02-03 21:50:29 -0500

    @Tim *Continued Hardware Interface Changes. *Still Operable *Kinect almost reinstated

--- AnubisClient/AnubisClient/AnubisCORE/Kine/KinectManager.cs
+++ AnubisClient/AnubisClient/AnubisCORE/Kine/KinectManager.cs
@@ -2,10 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Kinect;
+using System.IO;
+using AnubisClient.D_Hardware;
 
 namespace AnubisClient.AnubisCORE.Kine
 {
-    class KinectManager
+    class KinectManager:HardwareInterface
     {
+        private List<KinectInterface> KinectSens;
+        public KinectManager()
+        {
+            KinectSens = new List<KinectInterface>();
+        }
+
+        public override bool detectDevice()
+        {
+            foreach (var potential in KinectSensor.KinectSensors)
+            {
+                if (potential.Status == KinectStatus.Connected)
+                {
+                    KinectSens.Add(new KinectInterface(potential));
+                }
+            }
+
+            if (KinectSens.Count >= 1) 
+            {
+                return true;
+            }
+            return false;
+            
+        }
+
+        public override void startDeviceServer()
+        {
+            foreach (var Sensor in KinectSens)
+            {
+                Sensor.startDeviceServer();
+            }
+        }
+
+        public override void modifyModel(SkeletonRep mod)
+        {
+            switch (KinectSens.Count)
+            {
+                case 1: break; 
+                case 2: break; 
+                case 3: break;
+                case 4: break;
+            }
+        }
+
+        public override string getIdentString()
+        {
+            return "KinectManager";
+        }
+
+
     }
+
 }
---
+++ AnubisClient/AnubisClient/D_Hardware/KinectInterface.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using System.IO;
+
+namespace AnubisClient.D_Hardware
+{
+    class KinectInterface:HardwareInterface
+    {
+        KinectSensor Sense;
+        SkeletonRep SR;
+        public KinectInterface(KinectSensor sensor)
+        {
+            Sense = sensor;
+            SR = new SkeletonRep();
+        }
+
+        public override bool detectDevice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string getIdentString()
+        {
+            return "KinectSensor";
+        }
+
+        public override void startDeviceServer()
+        {
+            if (Sense != null)
+            {
+                Sense.SkeletonStream.Enable();
+                Sense.SkeletonFrameReady += Sense_SkeletonFrameReady;
+
+                try
+                {
+                    Sense.Start();
+                }
+                catch (IOException)
+                {
+
+                }
+            }
+        }
+
+        void Sense_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    //Copy Skeleton data to placeholder
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+
+                //Update joint position information
+                if (skeletons.Length != 0)
+                {
+                    if (skeletons[0].TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        JointCollection jnts = skeletons[0].Joints;
+
+                        float LDX = jnts[JointType.ElbowLeft].Position.X - jnts[JointType.ShoulderLeft].Position.X;
+                        float LDY = jnts[JointType.ElbowLeft].Position.Y - jnts[JointType.ShoulderLeft].Position.Y;
+                        double LAP = Math.Atan(LDY / LDX) * (180 / Math.PI);
+                        SR.ShoulderLeft.Pitch = LAP;
+
+                        float RLDZ = jnts[JointType.ElbowLeft].Position.Z - jnts[JointType.ShoulderLeft].Position.Z;
+                        float RLDY = jnts[JointType.ElbowLeft].Position.Y - jnts[JointType.ShoulderLeft].Position.Y;
+                        double RLAP = 180 - (Math.Atan(RLDY / RLDZ) * (180 / Math.PI));
+                        SR.ShoulderLeft.Roll = RLAP;
+
+                        float RDX = jnts[JointType.ElbowRight].Position.X - jnts[JointType.ShoulderRight].Position.X;
+                        float RDY = jnts[JointType.ElbowRight].Position.Y - jnts[JointType.ShoulderRight].Position.Y;
+                        double RAP = Math.Atan(RDY / RDX) * (180 / Math.PI) + 180;
+                        SR.ShoulderLeft.Pitch = RAP;
+
+                        float RRDZ = jnts[JointType.ElbowRight].Position.Z - jnts[JointType.ShoulderRight].Position.Z;
+                        float RRDY = jnts[JointType.ElbowRight].Position.Y - jnts[JointType.ShoulderRight].Position.Y;
+                        double RRAP = Math.Atan(RRDY / RRDZ) * (180 / Math.PI);
+                        SR.ShoulderLeft.Roll = RRAP;
+                    }
+                }
+            }
+        }
+
+        public override void modifyModel(SkeletonRep mod)
+        {
+            mod = SR;
+        }
+    }
+}
