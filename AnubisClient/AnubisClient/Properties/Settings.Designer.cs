519fc4e3329f1bf29af37f922ec72fd36c38ddbd
Dusoleil <HowCanSocksBeReal@gmail.com>
2016-04-07 14:47:26 -0400

    Removing old settings constant that isn't needed anymore

--- AnubisClient/AnubisClient/Properties/Settings.Designer.cs
+++ AnubisClient/AnubisClient/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace AnubisClient.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,18 +20,6 @@ namespace AnubisClient.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("3")]
-        public int NumberofKinects {
-            get {
-                return ((int)(this["NumberofKinects"]));
-            }
-            set {
-                this["NumberofKinects"] = value;
             }
         }
     }
